e70a73f007f182f0c89f1d8475a6719dd0c63671
Adam Steele <asteeledesign@gmail.com>
2026-03-31 16:05:13 -0400

    Fix import statement for connection in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Toaster } from 'react-hot-toast'
-import { connection } from 'next/server
+import { connection } from 'next/server'
   
 const inter = Inter({
   subsets: ['latin'],
